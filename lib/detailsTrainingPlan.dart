4ade34d425c1a98edfebe01e606abf8889516f0a
pedro-a22003407 <santospedro.2002@gmail.com>
2023-06-14 01:06:24 +0100

    backup5

--- lib/detailsTrainingPlan.dart
+++ lib/detailsTrainingPlan.dart
@@ -1,22 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:video_player/video_player.dart';
+import 'video_player_widget.dart';
 
-class DetailsTrainingPlan extends StatelessWidget {
+class DetailsTrainingPlan extends StatefulWidget {
   final int index;
-
   const DetailsTrainingPlan(this.index);
 
   @override
+  _DetailsTrainingPlanState createState() => _DetailsTrainingPlanState();
+}
+
+class _DetailsTrainingPlanState extends State<DetailsTrainingPlan> {
+  final asset = 'assets/bmwTest.mp4';
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = VideoPlayerController.asset(asset)
+      ..addListener(() => setState(() {}))
+      ..setLooping(true)
+      ..initialize().then((_) => controller.play());
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Detalhes de exercício'),
-      ),
-      body: Center(
-        child: Text(
-          'The details Page #$index',
-          style: const TextStyle(fontSize: 32.0),
-        ),
-      ),
+    final isMuted = controller.value.volume == 0;
+    return Column(
+      children: [
+        VideoPlayerWidget(controller: controller),
+        const SizedBox(height: 32),
+        if (controller != null && controller.value.isInitialized)
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.red,
+            child: IconButton(
+              icon: Icon(
+                isMuted ? Icons.volume_mute : Icons.volume_up,
+                color: Colors.white,
+              ),
+              onPressed: () => controller.setVolume(isMuted ? 1 : 0),
+            ),
+          ),
+      ],
     );
   }
 }
