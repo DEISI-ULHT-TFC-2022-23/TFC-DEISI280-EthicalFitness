a32db285eae3cc7670c6c09c832bc753c9bcb33d
pedro-a22003407 <santospedro.2002@gmail.com>
2023-06-15 02:29:36 +0100

    BackUp 7

--- lib/homePage2.dart
+++ lib/homePage2.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class HomePage2 extends StatefulWidget {
   const HomePage2({Key? key}) : super(key: key);
@@ -26,11 +27,13 @@ class _HomePage2State extends State<HomePage2> {
   }
 
   void scrollToFirstNews() {
-    _scrollController.animateTo(
-      0,
-      duration: const Duration(milliseconds: 100),
-      curve: Curves.easeInOut,
-    );
+    Timer(const Duration(seconds: 3), () {
+      _scrollController.animateTo(
+        720,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      );
+    });
   }
 
   @override
@@ -59,48 +62,174 @@ class _HomePage2State extends State<HomePage2> {
             ),
           ),
           ListView.builder(
-            controller: _scrollController,
-            itemCount: 5,
-            itemBuilder: (context, index) {
-              if (index == 0) {
-                return SizedBox(height: MediaQuery.of(context).size.height);
-              }
-              return Container(
-                width: MediaQuery.of(context).size.width,
-                height: 200,
-                color: Colors.grey,
-                margin: const EdgeInsets.only(bottom: 10),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
+              controller: _scrollController,
+              itemCount: 7, // Atualizado para exibir 7 notícias (2 adicionadas)
+              itemBuilder: (context, index) {
+                if (index == 0) {
+                  return SizedBox(height: MediaQuery.of(context).size.height);
+                }
+                if (index == 1) {
+                  // Primeira notícia
+                  return Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        'Título da notícia ${(index - 1)}',
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                      AnimatedContainer(
+                        alignment: Alignment.center,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeInOut,
+                        padding: const EdgeInsets.all(16.0),
+                        child: const Text(
+                          'Posts',
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 242, 242, 242),
+                          ),
                         ),
                       ),
-                      const SizedBox(height: 8),
-                      Text(
-                        'Descrição da notícia ${(index - 1)}',
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 200,
+                        color: const Color.fromARGB(
+                            255, 0, 28, 51), // Cor da primeira notícia
+                        margin: const EdgeInsets.only(bottom: 20),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'Título da segunda notícia',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(height: 8),
+                              Text(
+                                'Descrição da segunda notícia',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ],
-                  ),
-                ),
-              );
-            },
-          ),
+                  );
+                }
+
+                if (index == 2) {
+                  // Primeira notícia
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 200,
+                    color: const Color.fromARGB(
+                        255, 136, 136, 136), // Cor da primeira notícia
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Título da segunda notícia',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Descrição da segunda notícia',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                if (index == 3) {
+                  // Segunda notícia
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 200,
+                    color: const Color.fromARGB(
+                        255, 0, 28, 51), // Cor da segunda notícia
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Título da terceira notícia',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Descrição da terceira notícia',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                if (index == 4) {
+                  // Primeira notícia
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 200,
+                    color: const Color.fromARGB(
+                        255, 136, 136, 136), // Cor da primeira notícia
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Título da quarta notícia',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Descrição da quarta notícia',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              }),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: scrollToFirstNews,
+        backgroundColor: const Color.fromARGB(255, 133, 0, 0),
         child: const Icon(Icons.arrow_downward),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
