f836b72a43dd594011d435b3d08b8be0f00c0cd2
pedro-a22003407 <santospedro.2002@gmail.com>
2023-06-24 13:11:13 +0100

    BackUp 16

--- lib/homePage2.dart
+++ lib/homePage2.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
 
 class HomePage2 extends StatefulWidget {
   const HomePage2({Key? key}) : super(key: key);
@@ -10,6 +12,7 @@ class HomePage2 extends StatefulWidget {
 }
 
 class _HomePage2State extends State<HomePage2> {
+  final asset = 'assets/bmwTest.mp4';
   final ScrollController _scrollController = ScrollController();
 
   @override
@@ -27,7 +30,7 @@ class _HomePage2State extends State<HomePage2> {
   }
 
   void scrollToFirstNews() {
-    Timer(const Duration(seconds: 2), () {
+    Timer(const Duration(seconds: 1), () {
       _scrollController.animateTo(
         720,
         duration: const Duration(milliseconds: 500),
@@ -66,10 +69,7 @@ class _HomePage2State extends State<HomePage2> {
               itemCount: 7, // Atualizado para exibir 7 notícias (2 adicionadas)
               itemBuilder: (context, index) {
                 if (index == 0) {
-                  return SizedBox(height: MediaQuery.of(context).size.height);
-                }
-                if (index == 1) {
-                  // Primeira notícia
+                  // Vídeo
                   return Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -90,38 +90,50 @@ class _HomePage2State extends State<HomePage2> {
                       Container(
                         width: MediaQuery.of(context).size.width,
                         height: 200,
-                        color: const Color.fromARGB(
-                            255, 0, 28, 51), // Cor da primeira notícia
+                        color: Colors.black, // Cor de fundo do vídeo
                         margin: const EdgeInsets.only(bottom: 20),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              Text(
-                                'Título da segunda notícia',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              SizedBox(height: 8),
-                              Text(
-                                'Descrição da segunda notícia',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
+                        child: const VideoPlayerWidget(
+                          videoAsset:
+                              'assets/bmwTest.mp4', // Caminho do vídeo local
                         ),
                       ),
                     ],
                   );
                 }
-
+                if (index == 1) {
+                  // Primeira notícia
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 200,
+                    color: const Color.fromARGB(
+                        255, 0, 28, 51), // Cor da primeira notícia
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Título da segunda notícia',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Descrição da segunda notícia',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
                 if (index == 2) {
                   // Primeira notícia
                   return Container(
@@ -190,40 +202,6 @@ class _HomePage2State extends State<HomePage2> {
                     ),
                   );
                 }
-                if (index == 4) {
-                  // Primeira notícia
-                  return Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 200,
-                    color: const Color.fromARGB(
-                        255, 136, 136, 136), // Cor da primeira notícia
-                    margin: const EdgeInsets.only(bottom: 20),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            'Título da quarta notícia',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            'Descrição da quarta notícia',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                }
               }),
         ],
       ),
@@ -233,6 +211,61 @@ class _HomePage2State extends State<HomePage2> {
         child: const Icon(Icons.arrow_downward),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class VideoPlayerWidget extends StatefulWidget {
+  final String videoAsset;
+
+  const VideoPlayerWidget({required this.videoAsset, Key? key})
+      : super(key: key);
+
+  @override
+  _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
+}
+
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
+  late ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeVideoPlayer();
+  }
+
+  @override
+  void dispose() {
+    _chewieController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _initializeVideoPlayer() async {
+    final videoPlayerController =
+        VideoPlayerController.asset(widget.videoAsset);
+
+    await videoPlayerController.initialize();
+
+    _chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      autoPlay: true,
+      looping: true,
+    );
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_chewieController != null &&
+        _chewieController.videoPlayerController.value.isInitialized) {
+      return Chewie(
+        controller: _chewieController,
+      );
+    }
+
+    return const Center(
+      child: CircularProgressIndicator(),
     );
   }
 }
